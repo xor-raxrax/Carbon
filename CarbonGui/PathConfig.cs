a141f203244040224763b6cfadbbada3b71c5d26
xor-raxrax <73951548+xor-raxrax@users.noreply.github.com>
2024-07-11 23:05:41 +0200

    Normal dll project name & removed pdb conflict

--- CarbonGui/PathConfig.cs
+++ CarbonGui/PathConfig.cs
@@ -4,7 +4,7 @@
 	{
 		public static readonly string UserDirectory = "workspace";
 		public static readonly string Dump = "dumpresult.txt";
-		public static readonly string Dll = "CarbonDll.dll";
+		public static readonly string Dll = "Carbon.dll";
 		public static readonly string AddressDumper = "AddressDumper.exe";
 		public static readonly string Settings = "Settings.cfg";
 		public static readonly string Scripts = "scripts";
